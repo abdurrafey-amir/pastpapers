994e5c66d3c166306dcec20bf89427a3691548d0
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2025-10-03 22:10:51 +0500

    build issues

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,28 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
+app = Flask(
+    __name__,
+    static_folder="../frontend/dist",   # where your React build will go
+    static_url_path="/"
+)
 
-app = Flask(__name__)
 CORS(app)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pastpapers.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-from flask import Flask, send_from_directory
-import os
-
-app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
-
+# React frontend route
 @app.route("/")
 def serve_react():
     return send_from_directory(app.static_folder, "index.html")
 
-
-
+# Import routes AFTER app + db are created
 from routes import *
-
 
 if __name__ == '__main__':
     app.run(debug=True)
